10f240287d96211de218ee6b0e35232e6ba8f95c
Sylwia <sylwia.szalacha1@gmail.com>
2024-02-01 22:10:15 +0100

    first commit

---
+++ src/main/java/pl/sylwias/javafxopenai/Launcher.java
@@ -0,0 +1,7 @@
+package pl.sylwias.javafxopenai;
+
+public class Launcher {
+    public static void main(String[] args) {
+        ProductListApplication.main(args);
+    }
+}
---
+++ src/main/java/pl/sylwias/javafxopenai/RecommendationController.java
@@ -0,0 +1,27 @@
+package pl.sylwias.javafxopenai;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.util.List;
+
+public class RecommendationController {
+    @FXML
+    private Label recommendationLabel;
+
+    private List<String> products;
+    private OpenAiHelper openAiHelper;
+
+    public void initializeWithData(List<String> products) {
+        this.openAiHelper = new OpenAiHelper();
+        this.products = products;
+    }
+
+    @FXML
+    protected void recommend() {
+        String recommendation = openAiHelper.recommend(products);
+
+        recommendationLabel.setText(recommendation);
+    }
+
+}
